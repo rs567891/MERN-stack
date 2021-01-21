d20ed801ddba124d7ab2bbb7e4545fc3da7b379a
rs567891 <77713866+rs567891@users.noreply.github.com>
2021-01-21 13:00:26 +0530

    a full MERN stack project

--- Frontend With React/src/Home.jsx
+++ Frontend With React/src/Home.jsx
@@ -5,8 +5,6 @@ import Popper from 'popper.js';
 import $ from 'jquery';
 
 import { Route, Switch } from "react-router-dom";
-import About from "./About";
-import Contact from "./Contact";
 import Main from "./Main";
 import Login from "./Login";
 import Signup from "./Signup";
@@ -21,10 +19,9 @@ const Home = () => {
 
      <Switch>
      <Route exact path="/" component={Front} />
-        <Route exact path="/about" component={About} />
+        
         <Route exact path="/blog" component={Blog} />
         <Route exact path="/contactus" component={Contactus} />
-        <Route exact path="/contact" component={Contact}/>
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Signup }/>
  
